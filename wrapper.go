08f436594f01fd0256776351ee2c8d53bf5a9e16
Milad <mindwingx@gmail.com>
2023-08-24 15:14:15 +0200

    [branch][master] Init

---
+++ wrapper.go
@@ -0,0 +1,49 @@
+package viperwrapper
+
+import (
+	"errors"
+	"fmt"
+	"github.com/mindwingx/abstraction"
+	registry "github.com/spf13/viper"
+	"os"
+)
+
+type viper struct {
+	*registry.Viper
+}
+
+func New() abstraction.Registry {
+	return &viper{registry.New()}
+}
+
+func (v *viper) InitRegistry(configType string, configFilePath string) (err error) {
+	v.SetConfigType(configType)
+
+	config, err := os.Open(configFilePath)
+
+	if err != nil {
+		err = errors.New(fmt.Sprintf("failed to load config file: %s", err.Error()))
+		return err
+	}
+
+	err = v.ReadConfig(config)
+	if err != nil {
+		err = errors.New(fmt.Sprintf("failed to load config file: %s", err.Error()))
+		return err
+	}
+	return nil
+}
+
+func (v *viper) ValueOf(key string) abstraction.Registry {
+	return &viper{v.Sub(key)}
+}
+
+func (v *viper) Parse(item interface{}) (err error) {
+	err = v.Unmarshal(&item)
+	if err != nil {
+		err = errors.New(fmt.Sprintf("failed to load config file: %s", err.Error()))
+		return err
+	}
+
+	return nil
+}
---
+++ wrapper_test.go
@@ -0,0 +1,122 @@
+package viperwrapper
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestInitRegistry(t *testing.T) {
+	fileName := "config.yml"
+	currentDir, _ := os.Getwd()
+	filePath := fmt.Sprintf("%s/%s", currentDir, fileName)
+	file, err := os.Create(fileName)
+
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+
+	defer file.Close()
+
+	// instantiate
+	v := New()
+
+	// config file exists
+	err = v.InitRegistry("yml", filePath)
+	assert.Nil(t, err)
+
+	err = os.Remove(filePath)
+	if err != nil {
+		fmt.Println("Error deleting file:", err)
+		return
+	}
+
+	// config not file exists
+
+	err = v.InitRegistry("json", "nonexistent.json")
+	assert.NotNil(t, err)
+}
+
+func TestParse(t *testing.T) {
+	fileName := "config.yml"
+	data := "test:\n  v1: k1\n  v2: k2"
+	currentDir, _ := os.Getwd()
+	filePath := fmt.Sprintf("%s/%s", currentDir, fileName)
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+
+	defer file.Close()
+
+	_, err = file.WriteString(data)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	// instantiate
+	v := New()
+
+	// config file exists
+	err = v.InitRegistry("yml", filePath)
+
+	// Test with valid config file
+	var parsedData map[string]interface{}
+	err = v.Parse(&parsedData)
+	assert.Nil(t, err)
+	assert.Equal(t, "k1", parsedData["test"].(map[string]interface{})["v1"].(string))
+	assert.Equal(t, "k2", parsedData["test"].(map[string]interface{})["v2"].(string))
+
+	err = os.Remove(filePath)
+	if err != nil {
+		fmt.Println("Error deleting file:", err)
+		return
+	}
+}
+
+func TestValueOfSpecificKeyValues(t *testing.T) {
+	fileName := "config.yml"
+	data := "test:\n  v1: k1\n  v2: k2\nnew:\n  item: value\n  next: one"
+	currentDir, _ := os.Getwd()
+	filePath := fmt.Sprintf("%s/%s", currentDir, fileName)
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+
+	defer file.Close()
+
+	_, err = file.WriteString(data)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	// instantiate
+	v := New()
+
+	// config file exists
+	err = v.InitRegistry("yml", filePath)
+
+	// Test with valid config file
+	var parsedData map[string]interface{}
+
+	val := v.ValueOf("new")
+	err = val.Parse(&parsedData)
+	assert.Nil(t, err)
+	assert.Equal(t, "value", parsedData["item"].(string))
+	assert.Equal(t, "one", parsedData["next"].(string))
+
+	err = os.Remove(filePath)
+	if err != nil {
+		fmt.Println("Error deleting file:", err)
+		return
+	}
+}
